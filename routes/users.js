43176ec039a8494976577cfe02c10cdc0237c82c
GeoFro <georgeharoldfrost@gmail.com>
2018-02-12 16:12:36 +1100

    Added comments and new route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// Add new routes in here.
+// Routes added here will have the prefix "/users/"
+
+// Example of a new route at "/users/cool"
+router.get('/cool', function(req, res, next) {
+  res.send("You're a cool user");
+});
+
+// Exporting this module allows it to be accessed by app.js
 module.exports = router;
