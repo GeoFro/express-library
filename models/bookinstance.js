af959abd1122480a3453a581d19243348827cd28
GeoFro <georgeharoldfrost@gmail.com>
2018-02-12 21:00:09 +1100

    Fixed issue with BookInstanceSchema

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 var BookInstanceSchema = new Schema({
   book: { type: Schema.ObjectId, ref: 'Book', required: true },
   imprint: { type: String, required: true},
-  status: {type: String, required: true, enum: ['Available', 'Damaged', 'Borrowed', 'Reserved']},
+  status: {type: String, required: true, enum: ['Available', 'Damaged', 'Borrowed', 'Reserved'], default: 'Damaged'},
   due_back: {type: Date, default: Date.now}
 });
 
