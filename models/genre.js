50fe1fa7e798c732f528eece1119d270c32d151e
GeoFro <georgeharoldfrost@gmail.com>
2018-02-12 18:14:39 +1100

    Added async and fixed schema typo

--- models/genre.js
+++ models/genre.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 
-var Schema = mongoose.Schema:
+var Schema = mongoose.Schema;
 
 var GenreSchema = new Schema({
   name: {type: String, required: true, min: 3, max: 100}
